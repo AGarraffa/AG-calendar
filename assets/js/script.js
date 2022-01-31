be98bbc90c08aab320b3a39eafc89a4de69fd16d
Alfred Garraffa <agarraffa@gmail.com>
2022-01-31 10:07:42 -0700

    added code to auto style the bg color of each planner block

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,13 @@ console.log(`Today is ${day}`);
 var time = date.format('LT');
 console.log(`The current time is ${time}`);
 
-$('#current-time').text(`Today is ${today}, a ${day}. The time is ${time}.`)
+$('#current-time').text(`Today is ${day} ${today}. The time is ${time}.`)
+
+// array for all the div classes
+const appt = $('div.event');
+
+// gets the hour of the day (out of 24). Since the day starts at 7, subtract 7 from the hour to identify which div to style
+var hour = moment().hour() - 7;
 
 
 // Psuedo code
@@ -26,3 +32,24 @@ $('#current-time').text(`Today is ${today}, a ${day}. The time is ${time}.`)
     // if the hour is now, color is red
     // if the hour is future, color is green
     // do this via $(element).addClass("whatever") and have the classes styled in the css
+
+    // variable with getelementbyclassname yields an array with each element
+    // forEach loop over these elements with an if statement to set the background color
+    // use the array position +7 to identify time. The hour is given in 00-24
+
+
+// sets the current hour to red
+$(appt[hour]).css('background-color', 'red');
+
+// sets the other times to the respective colors
+// I wanted to use a foreach loop but it was giving me trouble.
+for (let i = 0; i < appt.length; i++) {
+
+    if (i < hour) {
+        $(appt[i]).css('background-color', 'gray');
+    }
+
+    if (i > hour) {
+        $(appt[i]).css('background-color', 'green');
+    }
+}
